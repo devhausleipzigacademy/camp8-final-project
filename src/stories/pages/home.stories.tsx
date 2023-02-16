3e8652482e4cd1812f4f1d687e3d4c571a7ee2f8
ArthiRagothaman <arthi@example.com>
2023-02-16 12:37:10 +0100

    testing storybook

---
+++ src/stories/pages/home.stories.tsx
@@ -0,0 +1,9 @@
+import Home from "../../pages/index";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+	title: "Pages/Home",
+	component: Home,
+};
+
+export const HomePage = () => <Home />;
