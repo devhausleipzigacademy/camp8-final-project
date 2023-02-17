cb48a183699566efa63fe769db3869d2f2d674c4
n1lsw <n1ls@pop-os.localdomain>
2023-02-17 17:01:59 +0100

    minor tweaks

--- src/pages/api/user/[num].ts
+++ src/pages/api/user/[num].ts
@@ -16,28 +16,5 @@ export default async function handler(
     return;
   }
 
-  //////
-  else if (request.method == "POST") {
-    const user = await prisma.user.create({
-      data: {
-        email: "elsa@prisma.io",
-        name: "Elsa Prisma",
-      },
-    });
-    //
-
-    response.status(200).json(user);
-    return;
-    // } else if (request.method == "DELETE") {
-    //   //
-    //   const deleteUser = await prisma.user.delete({
-    //     where: {
-    //       email: "elsa@prisma.io",
-    //     },
-    //   });
-    //   response.status(200).json(deleteUser);
-    //   return;
-  }
-
   response.status(404).send(`Invalid method: ${request.method}`);
 }
