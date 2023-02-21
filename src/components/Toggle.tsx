5b3cbaf5f12f15e2d08291ae69b5f1643bb8afa7
iameru <post@i3o.eu>
2023-02-21 12:16:57 +0100

    added headless UI to Toggle

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -1,3 +1,26 @@
-export default function Toggle() {
-  return <button>Toggle</button>;
+import { useState, Fragment } from "react";
+import { Switch } from "@headlessui/react";
+
+type Props = {
+  name: string;
+};
+export default function Toggle({ name }: Props) {
+  const [enabled, setEnabled] = useState(false);
+
+  return (
+    <Switch checked={enabled} onChange={setEnabled} as={Fragment}>
+      {({ checked }) => (
+        <button
+          className={`bg-gray-300 relative inline-flex h-6 w-12 items-center rounded-full border-2 border-purple-600`}
+        >
+          <span className="sr-only">Enable {name}</span>
+          <span
+            className={`${
+              checked ? "translate-x-5" : "translate-x-1"
+            } inline-block h-4 w-5 transform rounded-full bg-purple-600 transition`}
+          />
+        </button>
+      )}
+    </Switch>
+  );
 }
