9144c7bd15d2b9a060c06ec4f1d183e069b3649e
William de Vial <william.d.devial@gmail.com>
2023-02-21 16:09:00 +0100

    Update

--- serverPopulate.ts
+++ serverPopulate.ts
@@ -7,23 +7,25 @@ const bigNewData: NewData = newData;
 
 let i = 0;
 async function run() {
-  for (let i = 0; i < bigData.items.length && i < 5; i++) {
-    const realString = bigData.items[i].split(" ").join("_");
-    const temp: string = await axios
-      .get(
-        `https://api.spoonacular.com/food/ingredients/search?apiKey=94121dbecc2042ef8a2e5a3dd1873010&query=${realString}`
-      )
-      .then((res) => {
-        console.log(res.data.results[0].image);
-        return res.data.results[0].image;
-      })
-      .catch(() => "unknown");
-    bigNewData.Other.push({ image: temp, name: bigData.items[i] });
+  for (let i = 0; i < bigData.items.length && i < 100; i++) {
+    setInterval(async () => {
+      const realString = bigData.items[i].split(" ").join("_");
+      const temp: string = await axios
+        .get(
+          `https://api.spoonacular.com/food/ingredients/search?apiKey=ed4bdbe79fc24ac7ac5c21d18b7e185e&query=${realString}`
+        )
+        .then((res) => {
+          return res.data.results[0].image;
+        })
+        .catch(() => "unknown");
+
+      bigNewData.Other.push({ image: temp, name: bigData.items[i] });
+    }, 100);
   }
+  const holder = JSON.stringify(bigNewData);
+  fs.writeFileSync("./newData.json", holder);
 }
 run();
-const holder = JSON.stringify(bigNewData);
-fs.writeFileSync("./newData.json", holder);
 
 async function test(inputs: string[], categories: string) {}
 
