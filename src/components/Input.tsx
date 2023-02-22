5ae5fdc7421876f9e65a3f631400ea640658947f
kika <franfonsecav@gmail.com>
2023-02-22 11:33:10 +0100

    centered the text

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,4 +1,4 @@
-import { EnvelopeIcon, LockClosedIcon } from "@heroicons/react/24/solid";
+import { EyeIcon } from "@heroicons/react/24/solid";
 
 type InputProps = {
 	type: string;
@@ -8,23 +8,23 @@ export default function Input(props: InputProps) {
 	let placeholder = "";
 
 	if (props.type === "email") {
-		placeholder = "your@email.com";
+		placeholder = "Email";
 	} else if (props.type === "password") {
-		placeholder = "enter your password";
+		placeholder = "Password";
 	}
 
 	return (
 		<>
 			<label className="relative">
 				<input
-					className="flex items-center gap-3 pr-8 border border-[#7751C7] focus:border-[#7751C7]focus:border-2 invalid:border-red-500 ring-[#7751C7] rounded-md py-3 px-5 w-full"
+					className="flex items-center text-center gap-3 pr-8 border border-[#7751C7] focus:border-[#7751C7]focus:border-2 invalid:border-red-500 ring-[#7751C7] rounded-md py-3 px-5 w-full"
 					type={props.type}
 					placeholder={placeholder}
 				/>
 				{props.type === "email" ? (
 					""
 				) : (
-					<LockClosedIcon className="absolute right-0 flex h-full top-0 mr-2 justify-center w-6 aspect-square " />
+					<EyeIcon className="absolute right-0 flex h-full top-0 mr-2 justify-center w-6 aspect-square " />
 				)}
 			</label>
 		</>
