134c1975420658ca670403fc0bafe374bce3f402
William de Vial <william.d.devial@gmail.com>
2023-02-22 11:53:16 +0100

    Working more, need to collect API keys

--- serverPopulate.ts
+++ serverPopulate.ts
@@ -4,44 +4,31 @@ import axios from "axios";
 import * as fs from "fs";
 const bigData: Data = data;
 const bigNewData: NewData = newData;
-
-let i = 0;
-async function run() {
-  for (let i = 0; i < bigData.items.length && i < 100; i++) {
-    setInterval(async () => {
-      const realString = bigData.items[i].split(" ").join("_");
-      const temp: string = await axios
-        .get(
-          `https://api.spoonacular.com/food/ingredients/search?apiKey=ed4bdbe79fc24ac7ac5c21d18b7e185e&query=${realString}`
-        )
-        .then((res) => {
-          return res.data.results[0].image;
-        })
-        .catch(() => "unknown");
-
-      bigNewData.Other.push({ image: temp, name: bigData.items[i] });
-    }, 100);
-  }
-  const holder = JSON.stringify(bigNewData);
-  fs.writeFileSync("./newData.json", holder);
-}
-run();
-
-async function test(inputs: string[], categories: string) {}
-
-async function getLinks(vegetable: string) {
-  console.log("Hello World");
-  return "Hello";
-  return await axios
+let i = 100;
+const bla = setInterval(async function () {
+  const realString = bigData.items[i].split(" ").join("_");
+  const temp: string = await axios
     .get(
-      `https://api.spoonacular.com/food/ingredients/search?apiKey=94121dbecc2042ef8a2e5a3dd1873010&query=${vegetable}`
+      `https://api.spoonacular.com/food/ingredients/search?apiKey=5531584e9a644c37a7d70e8f59bc8ed6&query=${realString}`
     )
     .then((res) => {
-      console.log(res.data.results[0].image);
       return res.data.results[0].image;
     })
     .catch(() => "unknown");
-}
+
+  bigNewData.Other.push({
+    image: "https://spoonacular.com/cdn/ingredients_100x100/" + temp,
+    name: bigData.items[i],
+  });
+  console.log("added item:", bigData.items[i]);
+  i++;
+  if (i === 200) {
+    const holder = JSON.stringify(bigNewData);
+    fs.writeFileSync("./newData.json", holder);
+    clearInterval(bla);
+  }
+}, 1000);
+
 type NewData = {
   Other: Temp[];
 };
