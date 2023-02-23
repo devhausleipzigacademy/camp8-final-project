f1622c007c5d1b60bf4733644e09952fd80bd6c1
William de Vial <william.d.devial@gmail.com>
2023-02-23 11:52:14 +0100

    New branch

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{js,ts,jsx,tsx}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {},
+  plugins: [],
 };
+
+// /** @type {import('tailwindcss').Config}
+// module.exports = {
+// 	content: ["./src/**/*.{js,ts,jsx,tsx}"],
+// 	theme: {
+// 		fontFamily: {
+// 			DEFAULT: [""],
+// 		},
+// 		colors: {
+// 			primary: {
+// 				DEFAULT: "bg-gradient-to-b from-purple-600 to-blue-600",
+// 				transparent: "",
+// 				background:
+// 					"bg-[linear-gradient(165.25deg, rgba(0, 20, 255, 0.1) 12.95%, rgba(51, 0, 255, 0.1) 41.77%, rgba(54, 159, 255, 0.1) 64.82%, rgba(79, 190, 255, 0.1) 82.59%, rgba(172, 255, 180, 0.1) 97.03%), #FFFFFF;]",
+// 			},
+// 			secondary: {
+// 				DEFAULT: "#BEDCFA",
+// 				transparent: "",
+// 			},
+// 			white: {
+// 				DEFAULT: "",
+// 				transparent: "",
+// 			},
+// 			black: {
+// 				DEFAULT: "",
+// 			},
+// 			ux: {
+// 				error: "",
+// 				inactive: "",
+// 				success: "",
+// 			},
+// 		},
+// 		extend: {},
+// 	},
+// 	plugins: [require("@tailwindcss/line-clamp")],
+// };
