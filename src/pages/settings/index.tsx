6d329f5d4a4857df30c44047253076074a0422cb
William de Vial <william.d.devial@gmail.com>
2023-02-24 14:11:24 +0100

    Create New Account done

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -9,9 +9,28 @@ export default function Settings() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className="text-red-600 text-4xl font-bold flex justify-center">
-        Settings{" "}
-      </h1>
+      <div className="h-screen flex flex-col">
+        <div className="w-full flex items-center pl-8 pt-8">
+          <div className="bg-slate-400 h-8 w-8">Back</div>
+        </div>
+        <div className=" px-8 py-5 w-full flex-grow flex flex-col justify-around">
+          <div className="flex bg-slate-400 w-full h-10">
+            Account vs settings
+          </div>
+          <div className="flex w-full flex-col items-center gap-5">
+            <div className="bg-slate-400 h-10 w-10">Profile pic</div>
+            <div className="w-full flex flex-col gap-5">
+              <div className="bg-slate-400 h-20 w-full">Username</div>
+              <div className="bg-slate-400 h-20 w-full">Email</div>
+              <div className="bg-slate-400 h-20 w-full">Password</div>
+              <div className="bg-slate-400 h-20 w-full">Password</div>
+            </div>
+          </div>
+          <div className="flex bg-slate-400 w-full h-12">
+            Create new account
+          </div>
+        </div>
+      </div>
     </>
   );
 }
