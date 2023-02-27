b9b5044790444698af929e8f9b1bd2fdb4aeb157
acz1992 <116277029+acz1992@users.noreply.github.com>
2023-02-27 10:54:43 +0100

    got rid of index changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,48 +17,6 @@ export default function Home() {
 				/>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-			<h1 className="text-title">Landing Page</h1>
-
-			<h1 className="text-primary">Landing Page</h1>
-
-			<h1 className="text-links">Landing Page</h1>
-
-			<h1 className="text-secondary">Landing Page</h1>
-
-			<h1 className="cards-title">Landing Page</h1>
-
-			<h1 className="button-bold">Landing Page</h1>
-			<h1 className="button-large">Landing Page</h1>
-
-			<div className="flex flex-row">
-				<div className="h-96 aspect-square bg-primary-default-background"></div>
-				<div className="h-96 aspect-square bg-primary-default-Solid"></div>
-				<div className="h-96 aspect-square bg-primary-transparent"></div>
-				<div className="h-96 aspect-square bg-primary-frame"></div>
-			</div>
-
-			<hr />
-
-			<div className="flex flex-row">
-				<div className="h-96 aspect-square bg-secondary-default"></div>
-				<div className="h-96 aspect-square bg-secondary-transparent"></div>
-			</div>
-
-			<hr />
-
-			<div className="flex flex-row">
-				<div className="h-96 aspect-square bg-text-typo"></div>
-				<div className="h-96 aspect-square border-4 border-primary-default-Solid bg-text-white"></div>
-				<div className="h-96 aspect-square border-4 border-primary-default-Solid bg-text-white_transparent"></div>
-			</div>
-
-			<hr />
-
-			<div className="flex flex-row">
-				<div className="h-96 aspect-square bg-ux-error"></div>
-				<div className="h-96 aspect-square bg-ux-inactive"></div>
-				<div className="h-96 aspect-square bg-ux-success"></div>
-			</div>
 		</>
 	);
 }
