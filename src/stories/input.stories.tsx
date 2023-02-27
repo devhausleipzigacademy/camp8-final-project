1d5ba741775fe2db8b5d30a3d25004e67fca7b2c
kika <franfonsecav@gmail.com>
2023-02-27 17:15:20 +0100

    made some extra changes for the storybook to look nicer

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -1,28 +1,24 @@
-import { ComponentStory, Meta, StoryObj } from "@storybook/react";
+import {
+	ComponentMeta,
+	ComponentStory,
+	Meta,
+	StoryObj,
+} from "@storybook/react";
 import Input from "../components/Input";
 
-const meta: Meta<typeof Input> = {
+export default {
 	title: "components/Input",
 	component: Input,
-};
+	argTypes: {
+		type: {
+			options: ["email", "password"],
+			control: { type: "radio" },
+		},
+	},
+} as ComponentMeta<typeof Input>;
 
-export default meta;
+const Template: ComponentStory<typeof Input> = (args: any) => (
+	<Input {...args} />
+);
 
-type Story = StoryObj<typeof Input>;
-
-const Template: StoryObj<typeof Input> = (args: any) => <Input {...args} />;
-
-// const Template: ComponentStory<typeof Input> = (args) => <Input {...args} />;
-
-export const Email = Template.bind({});
-Email.args = {
-	type: "email",
-};
-export const Password = Template.bind({});
-Password.args = {
-	type: "password",
-};
-
-export const Default: Story = {};
-
-// export const Default: ComponentStory = {};
+export const inputField = Template.bind({});
