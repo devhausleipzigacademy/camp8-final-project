133285544dac1ba4b9c29cfec61a40ee8e7e3525
ArthiRagothaman <arthi@example.com>
2023-02-27 15:18:30 +0100

    update json files

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,10 @@
+const path = require("path");
+
 module.exports = {
-	stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+	stories: [
+		"../src/**/*.stories.mdx",
+		"../src/**/*.stories.@(js|jsx|ts|tsx)",
+	],
 	addons: [
 		"@storybook/addon-links",
 		"@storybook/addon-essentials",
@@ -8,5 +13,19 @@ module.exports = {
 	framework: "@storybook/react",
 	core: {
 		builder: "@storybook/builder-webpack5",
+	},
+	webpackFinal: async (config) => {
+		config.module.rules.push({
+			test: /\.css$/i,
+			use: [
+				{
+					loader: "postcss-loader",
+					options: { implementation: require.resolve("postcss") },
+				},
+			],
+			include: path.resolve(__dirname, "../"),
+		});
+		// Return the altered config
+		return config;
 	},
 };
