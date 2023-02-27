9f7169f36804feee182bee3a3973744519d150e0
ArthiRagothaman <arthi@example.com>
2023-02-27 22:23:53 +0100

    update fonts and text

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { useState, Fragment } from "react";
 import { Combobox } from "@headlessui/react";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
 type Items = Array<Item>;
 type Item = {
@@ -34,32 +35,29 @@ export function SearchBar() {
 		<div className=" fixed top-16">
 			<Combobox value={selectedItems} onChange={setSelectedItems}>
 				<div className=" relative mt-1">
-					<div className="relative w-full cursor-default overflow-hidden rounded-full bg-white text-end">
+					<div className="relative w-full cursor-default overflow-hidden rounded-full">
 						<Combobox.Input
-							className=" border  border-purple-300 rounded-full w-80 h-14 ml-8 p-5 focus:outline-none focus-visible  focus:border-purple-700"
+							className=" border border-primary-transparent rounded-full w-80 h-14 ml-8 p-5 focus:outline-none focus-visible  focus:border-primary-default-Solid"
 							displayValue={(i: Item) => i.name}
 							onChange={(event) => setQuery(event.target.value)}
 							placeholder={"Search"}
 						/>
 
 						<Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-8">
-							<Image
-								src="/SearchNormal.png"
-								alt="search"
-								width={18}
-								height={18}
-							/>
+							<MagnifyingGlassIcon width={18} height={18} />
 						</Combobox.Button>
 					</div>
 
-					<Combobox.Options className=" ui-absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base">
+					<Combobox.Options className=" ui-absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-secondary-transparent py-1 from-text-white_transparent">
 						{filteredItems.map((list) => (
 							<Combobox.Option
 								key={list.id}
 								value={list}
 								className={({ active }) =>
 									`relative cursor-default select-none py-2 pl-10 pr-4 ${
-										active ? "bg-teal-600 text-white" : " "
+										active
+											? " bg-primary-default-Solid text-text-white"
+											: " "
 									}`
 								}
 							>
@@ -68,8 +66,8 @@ export function SearchBar() {
 										<span
 											className={`block truncate ${
 												selected
-													? "font-medium"
-													: "font-normal"
+													? "font-sans"
+													: "font-thin"
 											}`}
 										>
 											{list.name}
@@ -78,8 +76,8 @@ export function SearchBar() {
 											<span
 												className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
 													active
-														? "text-white"
-														: "text-teal-600"
+														? "text-text-white"
+														: "text-primary-transparent"
 												}`}
 											></span>
 										)}
