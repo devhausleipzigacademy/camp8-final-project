885251cb65eab9032d20a5f18bd76ef33ced4d18
William de Vial <william.d.devial@gmail.com>
2023-02-27 16:03:31 +0100

    Finished basic setup

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,16 +1,45 @@
+import Image from "next/image";
+import { useState } from "react";
+import { BsQuestionSquare, BsCheck2 } from "react-icons/bs";
+import { IconContext } from "react-icons/lib";
+import { Tick } from "./Tick";
+
 type ListItemProps = {
   name: string;
   image: string;
+  quantity?: string;
+  checked: boolean;
+  onTick: () => void;
 };
 
 export default function ListItem(props: ListItemProps) {
+  const [checked, setChecked] = useState(false);
+  const onCheck = () => {
+    setChecked(!checked);
+    props.onTick();
+  };
   return (
     <>
-      <div className="flex flex-row w-full justify-between items-center">
-        <img className="h-12 w-12" src={props.image} alt="" />
-        <p>{props.name}</p>
-        <p>Quantity</p>
-        <button name="checkbox" className="h-10 w-10 bg-green-300"></button>
+      <div className="flex flex-row bg-primary-transparent border-2 border-text-typo max-w-[334px] h-16">
+        <img
+          className="h-full aspect-square"
+          src={
+            props.image
+              ? props.image
+              : "http://cdn.onlinewebfonts.com/svg/img_275679.png"
+          }
+          alt="http://cdn.onlinewebfonts.com/svg/img_275679.png"
+        />
+        <div className="flex p-2 justify-between items-center flex-grow">
+          <p className="text-text-typo text-primary">{props.name}</p>
+          <p className="text-secondary font-thin">{props.quantity}</p>
+          <div
+            onClick={onCheck}
+            className="h-10 w-10 bg-text-white border-primary-default-Solid rounded-2xl border-2 flex justify-center items-center"
+          >
+            {checked && <Tick classes="w-8 h-8 text-primary-default-Solid" />}
+          </div>
+        </div>
       </div>
     </>
   );
