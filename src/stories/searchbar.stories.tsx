6a6b72efb0930e5e785b69fa42869d64ce755519
ArthiRagothaman <arthi@example.com>
2023-02-27 16:03:37 +0100

    update

--- src/stories/searchbar.stories.tsx
+++ src/stories/searchbar.stories.tsx
@@ -1,5 +1,6 @@
-import { Meta, StoryObj } from "@storybook/react";
 import SearchBar from "@/components/SearchBar";
+import { Meta, StoryObj } from "@storybook/react";
+
 
 const meta: Meta<typeof SearchBar> = {
 	title: "components/searchBar",
