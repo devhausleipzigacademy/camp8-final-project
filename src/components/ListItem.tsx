5597957472abd815758364d7e1bd84f705474101
William de Vial <william.d.devial@gmail.com>
2023-03-01 15:43:34 +0100

    Fixed small issues

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -55,54 +55,52 @@ export default function ListItem(props: ListItemProps) {
   );
 
   return (
-    <>
-      <Transition
-        show={swiped}
-        appear={true}
-        enter="transform transition duration-[400ms]"
-        enterFrom="opacity-0 scale-y-0"
-        enterTo="opacity-100 scale-y-100"
-        leave="transform duration-[400ms] transition ease-in-out"
-        leaveFrom="opacity-100 scale-y-100 "
-        leaveTo="opacity-0 scale-y-0 "
+    <Transition
+      show={swiped}
+      appear={true}
+      enter="transform transition duration-[400ms]"
+      enterFrom="opacity-0 scale-y-0"
+      enterTo="opacity-100 scale-y-100"
+      leave="transform duration-[400ms] transition ease-in-out"
+      leaveFrom="opacity-100 scale-y-100 "
+      leaveTo="opacity-0 scale-y-0 "
+    >
+      <SwipeableListItem
+        className="max-w-[334px] h-16 bg-primary-transparent border border-secondary-default rounded-md flex flex-row"
+        leadingActions={leadingActions()}
+        trailingActions={trailingActions()}
+        threshold={0.5}
       >
-        <SwipeableListItem
-          className="max-w-[334px] h-16 bg-primary-transparent border-[1px] border-secondary-default rounded-md flex flex-row"
-          leadingActions={leadingActions()}
-          trailingActions={trailingActions()}
-          threshold={0.5}
-        >
-          <img
-            className="h-full aspect-square"
-            src={
-              props.image
-                ? props.image
-                : "http://cdn.onlinewebfonts.com/svg/img_275679.png"
-            }
-            alt="http://cdn.onlinewebfonts.com/svg/img_275679.png"
-          />
-          <div className="flex p-2 justify-between items-center flex-grow">
-            <p className="text-text-typo text-primary">{props.name}</p>
-            <p className="text-secondary font-thin">{props.quantity}</p>
-            <div
-              onClick={onCheck}
-              className="h-10 w-10 bg-text-white border-primary-default-Solid rounded-2xl border-2 flex justify-center items-center"
+        <img
+          className="h-full aspect-square"
+          src={
+            props.image
+              ? props.image
+              : "http://cdn.onlinewebfonts.com/svg/img_275679.png"
+          }
+          alt="http://cdn.onlinewebfonts.com/svg/img_275679.png"
+        />
+        <div className="flex p-2 justify-between items-center flex-grow">
+          <p className="text-text-typo text-primary">{props.name}</p>
+          <p className="text-secondary font-thin">{props.quantity}</p>
+          <div
+            onClick={onCheck}
+            className="h-10 w-10 bg-text-white border-primary-default-Solid rounded-2xl border-2 flex justify-center items-center"
+          >
+            <Transition
+              show={checked}
+              enter="transform transition duration-[400ms]"
+              enterFrom="opacity-0 rotate-[-120deg] scale-50"
+              enterTo="opacity-100 rotate-0 scale-100"
+              leave="transform duration-200 transition ease-in-out"
+              leaveFrom="opacity-100 rotate-0 scale-100 "
+              leaveTo="opacity-0 scale-95 "
             >
-              <Transition
-                show={checked}
-                enter="transform transition duration-[400ms]"
-                enterFrom="opacity-0 rotate-[-120deg] scale-50"
-                enterTo="opacity-100 rotate-0 scale-100"
-                leave="transform duration-200 transition ease-in-out"
-                leaveFrom="opacity-100 rotate-0 scale-100 "
-                leaveTo="opacity-0 scale-95 "
-              >
-                <Tick classes="w-8 h-8 text-primary-default-Solid" />
-              </Transition>
-            </div>
+              <Tick classes="w-8 h-8 text-primary-default-Solid" />
+            </Transition>
           </div>
-        </SwipeableListItem>
-      </Transition>
-    </>
+        </div>
+      </SwipeableListItem>
+    </Transition>
   );
 }
