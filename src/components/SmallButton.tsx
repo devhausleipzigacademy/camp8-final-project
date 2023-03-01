16f3dd544722bbf2f0b0f7fd20bcc7128d03778e
acz1992 <116277029+acz1992@users.noreply.github.com>
2023-03-01 20:01:37 +0100

    added some styling

--- src/components/SmallButton.tsx
+++ src/components/SmallButton.tsx
@@ -14,12 +14,9 @@ export interface SmallButtonProps
 export function SmallButton({ label, disabled, ...props }: SmallButtonProps) {
 	return (
 		<button
-			className={clsx(
-				"flex items-center text-center border-box rounded-lg w-full h-12 disabled:cursor-not-allowed",
-				variant == "primary"
-					? " bg-gradient-bl-primary-background button-large justify-around text-text-white disabled:bg-ux-inactive active:shadow-primaryButtonShadow"
-					: "bg-text-white border-2 button-bold justify-center text-primary-default-Solid disabled:text-ux-inactive active:shadow-secondaryButtonShadow"
-			)}
+			className={
+				"text-center rounded-md w-32 h-44 disabled:cursor-not-allowed"
+			}
 			disabled={disabled}
 			{...props}
 		>
