f8d497dfd79640646a8e3c23a9f553cb0eb1c21e
n1lsw <n1ls@pop-os.localdomain>
2023-03-01 16:35:16 +0100

    Added the features per issue 66

---
+++ src/pages/api/autocomplete.ts
@@ -0,0 +1,52 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { z, ZodError } from "zod";
+import { MasterItem, List, Prisma, PrismaClient } from "@prisma/client";
+var stringSimilarity = require("string-similarity");
+
+export function sortByRating(input: Array<{ target: string; rating: number }>) {
+  const sorted = [...input];
+
+  sorted.sort((a, b) => {
+    if (a.rating < b.rating) {
+      return 0;
+    }
+    if (a.rating > b.rating) {
+      return -1;
+    }
+    return 0;
+  });
+  return sorted;
+}
+
+const prisma = new PrismaClient();
+
+const inputQuerySchema = z.object({
+  name: z.string(),
+});
+
+export default async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  if (request.method == "GET") {
+    const { name } = inputQuerySchema.parse(request.query);
+    const list = await prisma.masterItem.findMany({});
+    const item_names = list.map((item) => item.name);
+    const matches = stringSimilarity.findBestMatch(name, item_names);
+    const sorted_matches = sortByRating(matches.ratings);
+    const result = [];
+
+    for (let entry of sorted_matches) {
+      if (entry.rating > 0) {
+        result.push(entry.target);
+      }
+    }
+
+    response.status(200).json(result.slice(0, 5));
+    return;
+  }
+  response.status(404).send(`Invalid method: ${request.method}`);
+}
+
+// const Query = inputQuerySchema.parse(request.query); // "healed, in our case"
+// const db = prisma.item.findMany({}); // get all strings in items
