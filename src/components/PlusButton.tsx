edc16d58afa215560ed3ae29570ccdc129325cc5
peeler <t.delourme11@gmail.com>
2023-03-01 15:55:57 +0100

    button fixed

--- src/components/PlusButton.tsx
+++ src/components/PlusButton.tsx
@@ -6,13 +6,11 @@ type PlusButton = {
 
 export function PlusButton(props: PlusButton) {
   return (
-    <div>
-      <button
-        onClick={props.add}
-        className="text-text-white text-2xl bg-primary-default-Solid hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-full px-5 py-2.5 text-center mr-2 mb-2"
-      >
-        +
-      </button>
-    </div>
+    <button
+      onClick={props.add}
+      className="text-text-white text-2xl bg-primary-default-Solid  py-2 px-4 rounded-full"
+    >
+      +
+    </button>
   );
 }
