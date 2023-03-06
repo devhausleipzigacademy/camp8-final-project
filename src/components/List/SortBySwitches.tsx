18209722a3aebdbdd36abb033d37cb7983f2d64f
Thomas <sappeiduda@gmail.com>
2023-03-06 10:30:43 +0100

    some styling adjusted

--- src/components/List/SortBySwitches.tsx
+++ src/components/List/SortBySwitches.tsx
@@ -57,49 +57,47 @@ type SortByProps = {
 
 export function SortBySwitches(props: SortByProps) {
   return (
-    <>
-      <RadioGroup
-        className="font-sans flex flex-row justify-center items-center bg-secondary-transparent text-sm h-7 gap-2 rounded-[10px]"
-        value={props.sortBy}
-        onChange={props.setSort}
-      >
-        <RadioGroup.Option className="h-full w-1/3" value="date">
-          {({ checked }) => (
-            <p
-              className={clsx(
-                "h-full  flex justify-center items-center rounded-listitem",
-                checked && "bg-primary-default-background text-text-white"
-              )}
-            >
-              Date
-            </p>
-          )}
-        </RadioGroup.Option>
-        <RadioGroup.Option className="h-full w-1/3" value="category">
-          {({ checked }) => (
-            <p
-              className={clsx(
-                "h-full  flex justify-center items-center rounded-listitem",
-                checked && "bg-primary-default-background text-text-white"
-              )}
-            >
-              Category
-            </p>
-          )}
-        </RadioGroup.Option>
-        <RadioGroup.Option className="h-full w-1/3" value="alphabetical">
-          {({ checked }) => (
-            <p
-              className={clsx(
-                "h-full  flex justify-center items-center rounded-listitem",
-                checked && "bg-primary-default-background text-text-white"
-              )}
-            >
-              Alphabetical
-            </p>
-          )}
-        </RadioGroup.Option>
-      </RadioGroup>
-    </>
+    <RadioGroup
+      className="font-sans flex flex-row justify-center items-center bg-secondary-transparent text-sm h-7 rounded-listitem"
+      value={props.sortBy}
+      onChange={props.setSort}
+    >
+      <RadioGroup.Option className="h-full w-1/3" value="date">
+        {({ checked }) => (
+          <p
+            className={clsx(
+              "h-full w-full flex justify-center items-center rounded-listitem",
+              checked && "bg-primary-default-background text-text-white"
+            )}
+          >
+            Date
+          </p>
+        )}
+      </RadioGroup.Option>
+      <RadioGroup.Option className="h-full w-1/3" value="category">
+        {({ checked }) => (
+          <p
+            className={clsx(
+              "h-full w-full  flex justify-center items-center rounded-listitem",
+              checked && "bg-primary-default-background text-text-white"
+            )}
+          >
+            Category
+          </p>
+        )}
+      </RadioGroup.Option>
+      <RadioGroup.Option className="h-full w-1/3" value="alphabetical">
+        {({ checked }) => (
+          <p
+            className={clsx(
+              "h-full w-full flex justify-center items-center rounded-listitem",
+              checked && "bg-primary-default-background text-text-white"
+            )}
+          >
+            Alphabetical
+          </p>
+        )}
+      </RadioGroup.Option>
+    </RadioGroup>
   );
 }
