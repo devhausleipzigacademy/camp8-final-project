f86fe0771facc1e407fdabc0aebf59f1db442bd0
peeler <t.delourme11@gmail.com>
2023-03-07 14:34:51 +0100

    trying to get the card component

---
+++ src/components/PlusButtonList.tsx
@@ -0,0 +1,24 @@
+import { PlusButton } from "@/components/PlusButton";
+import React from "react";
+import { useState } from "react";
+
+type PlusButtonList = {};
+
+export function PlusButtonList(props: PlusButtonList) {
+  const [addList, setAddList] = useState(false);
+  function handleClick() {
+    setAddList(true);
+  }
+  return (
+    
+  )
+    
+      <PlusButton onClick={handleClick}></PlusButton>
+      addList && (
+      <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden">
+        <h1>new list</h1>
+      </div>
+      )
+    
+  
+}
