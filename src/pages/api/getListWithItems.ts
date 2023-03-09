e7b23e5490cfab015c3be329a57e9402caca9004
Thomas <sappeiduda@gmail.com>
2023-03-09 11:51:42 +0100

    prisma client

--- src/pages/api/getListWithItems.ts
+++ src/pages/api/getListWithItems.ts
@@ -1,12 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { z, ZodError } from "zod";
-import { Item, List, Prisma, PrismaClient } from "@prisma/client";
+import { prisma } from "./prisma";
 
 const inputQuerySchema = z.object({
   id: z.string(),
 });
-
-const prisma = new PrismaClient();
 
 export default async function handler(
   request: NextApiRequest,
