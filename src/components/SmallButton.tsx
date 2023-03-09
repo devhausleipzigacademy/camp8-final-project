a56015cb9346ca046357ac9df0755b5b7123460e
acz1992 <116277029+acz1992@users.noreply.github.com>
2023-03-09 10:56:38 +0100

    minimum width added to small button

--- src/components/SmallButton.tsx
+++ src/components/SmallButton.tsx
@@ -14,7 +14,7 @@ export function SmallButton({ label, disabled, ...props }: SmallButtonProps) {
 	return (
 		<button
 			className={
-				"enabled:bg-grad-default px-1 py-2.5 text-center text-text-white text-secondary rounded-md w-16 h-11 disabled:cursor-not-allowed disabled:bg-ux-inactive"
+				"enabled:bg-grad-default px-1 py-2.5 text-center text-text-white text-secondary min-w-min rounded-md w-16 h-11 disabled:cursor-not-allowed disabled:bg-ux-inactive"
 			}
 			disabled={disabled}
 			{...props}
