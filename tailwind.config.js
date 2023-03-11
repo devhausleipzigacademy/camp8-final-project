9e40a2ff4b40c29fc46a84b63bf39c0dd87aa41b
acz1992 <116277029+acz1992@users.noreply.github.com>
2023-03-11 15:27:16 +0100

    Dans requested changes made

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
 	content: ["./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		fontFamily: {
@@ -18,6 +17,7 @@ module.exports = {
 				default: "#BEDCFA",
 				transparent: "rgba(190, 223, 255, 0.25)",
 			},
+			card: { DEFAULT: "#eff7ff" },
 			text: {
 				typo: "#453C57",
 				white: "#FFFFFF",
@@ -28,6 +28,7 @@ module.exports = {
 				inactive: "#BEBEBE",
 				success: "#B4E747",
 			},
+			transparent: "transparent",
 		},
 		backgroundImage: {
 			"grad-default":
@@ -40,45 +41,9 @@ module.exports = {
 			secondaryButtonShadow:
 				"inset 3px 4px 0px rgba(134, 100, 196, 0.3);",
 		},
+		extend: {
+			transitionDuration: { 400: "400ms" },
+		},
 	},
 	plugins: [],
-=======
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    fontFamily: {
-      sans: ["Inter", "sans-serif"],
-      heading: ["Staatliches", "sans-serif"],
-    },
-    colors: {
-      primary: {
-        default: {
-          Solid: "#5F62C7",
-          background: "#7751c7",
-        },
-        transparent: "rgba(66, 17, 169, 0.1)",
-        frame: "#E6E8FF",
-      },
-      secondary: {
-        default: "#BEDCFA",
-        transparent: "rgba(190, 223, 255, 0.25)",
-      },
-      card: { DEFAULT: "#eff7ff" },
-      text: {
-        typo: "#453C57",
-        white: "#FFFFFF",
-        white_transparent: "rgba(255, 255, 255, 0.2)",
-      },
-      ux: {
-        error: "#EC1C00",
-        inactive: "#BEBEBE",
-        success: "#B4E747",
-      },
-      transparent: "transparent",
-    },
-    extend: {
-      transitionDuration: { 400: "400ms" },
-    },
-  },
-  plugins: [],
->>>>>>> main
 };
