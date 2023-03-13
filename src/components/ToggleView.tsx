824fde8f814e1dee59b0c4ddf0cebc63b17a373a
peeler <t.delourme11@gmail.com>
2023-03-13 19:00:13 +0100

    adding normal and big to the switch

--- src/components/ToggleView.tsx
+++ src/components/ToggleView.tsx
@@ -22,7 +22,7 @@ export function ToggleView(props: ToggleView) {
         <Switch
           checked={size}
           onChange={setSize}
-          className={`${
+          className={`w-full relative${
             size
               ? " text-primary-default-Solid  bg-secondary-transparent border-secondary-default"
               : "bg-secondary-transparent  text-primary-default-Solid  border-secondary-default"
@@ -30,15 +30,28 @@ export function ToggleView(props: ToggleView) {
           relative inline-flex h-full w-full content-center rounded-lg `}
         >
           <span className="sr-only ">Toggle size of components</span>
+          <div className="flex justify-around w-full relative text-text-white z-10 px-4">
+            <span
+              className={`${
+                size ? " text-primary-default-Solid" : "text-text-white"
+              }`}
+            >
+              Normal
+            </span>
+
+            <span
+              className={`${
+                !size ? " text-primary-default-Solid" : "text-text-white"
+              }`}
+            >
+              Big
+            </span>
+          </div>
           <span
             aria-hidden="true"
-            className={`${
-              size
-                ? "translate-x-full before:content-['Big'] text-text-white "
-                : "-translate-x-0 after:content-['Normal'] text-text-white "
-            }
-            pointer-events-none  inline-block h-full w-[50%] py-2 transform rounded-md bg-primary-default-Solid transition duration-300 ease-in-out `}
-          />
+            className={`absolute ${size ? "translate-x-full" : "-translate-x-0"}
+            pointer-events-none text-text-white inline-block h-full w-[50%] py-2 transform rounded-md bg-primary-default-Solid transition duration-300 ease-in-out `}
+          ></span>
         </Switch>
       </Switch.Group>
     </div>
