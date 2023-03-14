a02a68c2f4d96a676ed8d0501a72aff109a2fc4e
William de Vial <william.d.devial@gmail.com>
2023-03-14 15:01:02 +0100

    Backend Working

---
+++ src/pages/api/listItems.ts
@@ -0,0 +1,49 @@
+import { MasterItem } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+import { z, ZodError } from "zod";
+import { prisma } from "src/pages/api/prisma";
+
+/*
+ */
+export default async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  if (request.method === "GET") {
+    try {
+      const { inputList } = inputQueryTest.parse(request.query);
+
+      const list = await prisma.list.findFirst({
+        where: {
+          id: inputList,
+        },
+      });
+
+      const items = await prisma.category.findMany({
+        where: {
+          item: {
+            some: {
+              listIdentifier: inputList,
+            },
+          },
+        },
+        include: {
+          item: true,
+        },
+      });
+      const whatever = { listName: list?.listName, list: items };
+      response.status(200).send(whatever);
+    } catch (err) {
+      if (err instanceof ZodError) {
+        response.status(400).send(`Wrong Data Sent =>${JSON.stringify(err)}`);
+      } else {
+        response.status(418).send(JSON.stringify(err));
+      }
+    }
+    response.status(404).send(`Invalid method, need POST: ${request.method}`);
+  }
+}
+
+const inputQueryTest = z.object({
+  inputList: z.string(),
+});
--- src/pages/list/[slug]/index.tsx
+++ src/pages/list/[slug]/index.tsx
@@ -1,3 +1,33 @@
-export default function Home() {
-  return <div>I am a list</div>;
+import { GetServerSideProps } from "next";
+import axios from "axios";
+import { Item } from "@prisma/client";
+
+type Category = {
+  id: string;
+  name: string;
+  item: Item[];
+};
+
+type InputProps = {
+  listName: string;
+  list: Category[];
+};
+
+export default function Home(getData: InputProps) {
+  return <div></div>;
 }
+
+const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const slug = params?.slug; // slug = listId
+  const getData: InputProps = await axios
+    .get(`http://localhost:3000/api/listItems?inputList=${slug}`)
+    .then((res) => res.data);
+
+  return {
+    props: {
+      getData,
+    },
+  };
+};
+
+export { getServerSideProps };
