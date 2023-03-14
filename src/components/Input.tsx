e05186ed870557ce02aec003d1cde1744336e8ce
kika <franfonsecav@gmail.com>
2023-03-14 11:12:11 +0100

    updated component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,25 +1,13 @@
-import { EyeIcon } from "@heroicons/react/24/solid";
-
-type InputProps = {
-	type: string;
-};
-
-export default function Input(props: InputProps) {
-	let placeholder: string = props.type === "email" ? "Email" : "Password";
-
+export default function Input() {
 	return (
 		<>
 			<label className="relative">
 				<input
 					className="flex items-center text-center gap-3 pr-8 border border-primary-default-background focus:border-2 invalid:border-ux-error invalid:border-2
 					  rounded-md py-3 px-5 w-full"
-					type={props.type}
-					placeholder={placeholder}
+					type="email"
+					placeholder="Email"
 				/>
-
-				{props.type === "password" && (
-					<EyeIcon className="absolute right-0 flex h-full top-0 mr-2 justify-center w-6 aspect-square " />
-				)}
 			</label>
 		</>
 	);
--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -9,16 +9,8 @@ import Input from "../components/Input";
 export default {
 	title: "components/Input",
 	component: Input,
-	argTypes: {
-		type: {
-			options: ["email", "password"],
-			control: { type: "radio" },
-		},
-	},
 } as ComponentMeta<typeof Input>;
 
-const Template: ComponentStory<typeof Input> = (args: any) => (
-	<Input {...args} />
-);
+const Template: ComponentStory<typeof Input> = () => <Input />;
 
 export const inputField = Template.bind({});
