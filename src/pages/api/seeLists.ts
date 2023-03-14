53d0b428d39afce7004701c1619ddb7398894010
Amyria <54778894+amyria3@users.noreply.github.com>
2023-03-14 14:17:05 +0100

    update-SeeLists.ts according to Card-Component
    //Following issues: type Errors on Lines 57-59
    //52-54 returns the number of all items, not filtered by !checked

--- src/pages/api/seeLists.ts
+++ src/pages/api/seeLists.ts
@@ -1,6 +1,19 @@
+//This api-Endpoint will return an Array of Objects like this one:
+
+//export type ExampleObject = {
+//  id: string;
+//  listName: string;
+//  createdAt: String;
+//  itemsTotal: Number;
+//  itemsChecked: Number;
+//  favorite: boolean;
+//};
+
+
 import { NextApiRequest, NextApiResponse } from "next";
 import { z } from "zod";
 import { prisma } from "./prisma";
+
 
 const inputQuerySchema = z.object({
   id: z.string(),
@@ -11,19 +24,46 @@ export default async function handler(
   response: NextApiResponse
 ) {
   if (request.method === "GET") {
+
     // Parse the user ID from the query parameters
 
     const { id } = inputQuerySchema.parse(request.query);
 
-    // Find all lists that belong to the user
-    const lists = await prisma.list.findMany({
+    //First, get all the Ids that belong to a user, based on userIdentifier
+    const userListsHttpRespond = await prisma.list.findMany({
       where: {
         userIdentifier: id,
       },
+      select: {
+        id: true,
+        listName: true,
+        createdAt: true,
+        favorite: true,
+        items: {
+          select: {
+            checked: true,
+          },
+        },
+      },
     });
 
-    response.status(200).json(lists);
-    return;
-  }
+      userListsHttpRespond.forEach((element) => {
+
+      const itemsChecked = element.items.map((item) => {
+        item.checked === true
+      }).length
+
+      const itemsTotal = element.items.map((item)=>{}).length
+      element.itemsTotal = itemsTotal
+      element.itemsChecked = itemsChecked
+      delete element.items
+
+      console.log({ ...element})
+    }),
+
+    response.status(200).json(userListsHttpRespond)
+    console.log()
+    return userListsHttpRespond;
+  };
   response.status(405).send("not ok");
 }
