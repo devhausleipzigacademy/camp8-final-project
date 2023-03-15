2eaca320fd39ceafd615c7d3ed6a1cd14c1d1519
ArthiRagothaman <arthi@example.com>
2023-03-15 09:57:32 +0100

    email screen working

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,9 +1,9 @@
 export default function Input() {
 	return (
 		<>
-			<div className="relative">
+			<div className="flex">
 				<input
-					className="w-80 h-14 px-4 flex text-center m-3 border valid:border-primary-default-Solid valid:border-2 invalid:border-ux-error invalid:border-2
+					className="w-80 h-14 px-4 flex text-center m-3 border border-primary-default-Solid focus:outline-none  hover:border-primary-default-Solid active:border-primary-default-Solid invalid:border-ux-error invalid:border-2
 					  rounded-md "
 					type="email"
 					placeholder="Email"
