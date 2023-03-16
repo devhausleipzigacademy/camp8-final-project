69b2da441fe17c9182d440fc6e87a5322f4c5e01
ArthiRagothaman <arthi@example.com>
2023-03-16 14:43:21 +0100

    pages added

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -3,7 +3,7 @@ export default function Input() {
 		<>
 			<div className="flex">
 				<input
-					className="w-80 h-14 px-4 flex text-center m-3 border border-primary-default-Solid focus:outline-none  hover:border-primary-default-Solid active:border-primary-default-Solid invalid:border-ux-error invalid:border-2
+					className="w-80 h-14 flex text-center  border border-primary-default-Solid focus:outline-none  hover:border-primary-default-Solid active:border-primary-default-Solid invalid:border-ux-error invalid:border-2
 					  rounded-md "
 					type="email"
 					placeholder="Email"
