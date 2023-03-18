a0e8fed9ca68e0383b4e3f2a87a315f6c76fab0c
William de Vial <william.d.devial@gmail.com>
2023-03-18 12:16:57 +0100

    Merge from secret project with new schema

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,6 @@
 import data from "../data.json";
 import { PrismaClient } from "@prisma/client";
+import axios from "axios";
 type Input = {
   image: string;
   name: string;
@@ -9,6 +10,17 @@ const prisma = new PrismaClient();
 async function main() {
   const bigData: Data = data;
   for (let categoryName in bigData) {
+    await prisma.masterItem.createMany({
+      data: bigData[categoryName].map((x) => {
+        return {
+          approved: true,
+          imageUrl: x.image,
+          name: x.name,
+          category: categoryName,
+        };
+      }),
+      skipDuplicates: true,
+    });
     await prisma.category.create({
       data: {
         name: categoryName,
@@ -27,5 +39,53 @@ async function main() {
       },
     });
   }
+
+  const John = await prisma.user.create({
+    data: {
+      name: "John Doe",
+      email: "john.doe@email.com",
+      lists: {
+        create: {
+          listName: "John Doe's List",
+        },
+      },
+    },
+    include: {
+      lists: true,
+    },
+  });
+  const exampleItems = [
+    "apples",
+    "pears",
+    "bananas",
+    "oranges",
+    "grapes",
+    "strawberries",
+    "blueberries",
+    "beef",
+    "sausage",
+    "frankfurt",
+    "pork",
+    "yeast",
+    "cornstarch",
+    "sugar",
+    "risotto",
+    "semolina",
+    "cinnamon",
+    "shampoo",
+    "burrata",
+    "cajeta",
+    "quark",
+  ];
+  for (let i of exampleItems) {
+    await axios.post("http://localhost:3000/api/addItem", {
+      query: i,
+      inputList: John.lists[0].id,
+    });
+  }
+  console.log(
+    "For bubble chart go to page:\n",
+    `http://localhost:3000/list/${John.lists[0].id}/bubble`
+  );
 }
 main();
