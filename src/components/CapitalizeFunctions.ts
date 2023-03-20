6fa07418f8093d8d6c052782d598770e3bb3a299
Thomas <sappeiduda@gmail.com>
2023-03-20 14:38:21 +0100

    fix

--- src/components/CapitalizeFunctions.ts
+++ src/components/CapitalizeFunctions.ts
@@ -4,13 +4,12 @@ export function capitalizeWord(word: string) {
 
 export function capitalizeCategory(category: string) {
     let arr = category.split("-")
-    let word = ""
     for (let i = 0; i < arr.length; i++){
         
         if (arr[i] === "and"){
             continue
         }
-        word.charAt(0).toUpperCase() + word.slice(1);
+        arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
     }
-    return arr
+    return arr.join(" ")
 }
