92305daa78f75b1b02ab04431a91180c99609dac
amyria3 <54778894+amyria3@users.noreply.github.com>
2023-03-22 15:30:36 +0100

    What's new: react-query api requests work

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,10 +6,15 @@ import {
   LeadingActions,
 } from "react-swipeable-list";
 import clsx from "clsx";
-import axios from "axios";
 import { Trash, Bookmark } from "react-feather";
 import "react-swipeable-list/dist/styles.css";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
+
+//Issues:
+//fixed position of the card description, no rounding
+//swip-right falls back automatically
+//no animation / user feedback after delete-Function triggered
 
 ///TYPES
 export type UserList = {
@@ -22,59 +27,153 @@ export type UserList = {
 };
 export type CardProps = {
   data: UserList;
-  createNewCard: boolean;
+  new_card: boolean;
 };
 
 export type UserLists = Array<UserList>;
 
-export const example_list = {
-  id: "845d8198-7089-4618-891b-47a2b5038c83",
-  listName: "first custom list",
-  createdAt: "2023.03.03",
-  favorite: false,
-  itemsTotal: 0,
-  itemsChecked: 0,
+//define Types for Data we are going to send or receive
+type UpdateListNameRequestData = {
+  list_id: string;
+  newName: string;
 };
 
-export const example_card: CardProps = {
-  data: example_list,
-  createNewCard: false,
-};
-
-export function SingleCard({ data, createNewCard }: CardProps) {
-
+export function SingleCard({ data, new_card }: CardProps) {
   //current list Id
   const listId = data.id;
 
-  //binding to access favorite
-  const pinned = data.favorite
+  //make prop "new_card" usable / changable
+  const [createNewCard, setCreateNewCard] = useState(false);
+  // setCreateNewCard(new_card); //triggered rerender
 
-  //binding, to determine behavior of Card (Title turns into Inputfield, when changingName is set to true. changingName will be set to true on CLick.
-  const [changingName, setChangingName] = useState(false);
+  //link to queryClient in app.tsx
+  const queryClient = useQueryClient();
 
-  //define Types for Data we are going to send
-  type UpdateListNameRequestData = {
-    id: string;
-    newName: string;
-  };
+  //COMPONENT FUNCTIONS:
 
-  type DeleteListRequestData = {
-    id: string;
-  };
+  //1. <Delete list
+  function ApiDeleteList(list_id: string) {
+    return fetch(`http://localhost:3000/api/deleteList?id=${list_id}`, {
+      method: "DELETE",
+    }).then((response) => {
+      return response.json();
+    });
+  }
+
+  const { mutate: deleteList } = useMutation(
+    (list_id: string) => ApiDeleteList(list_id),
+    {
+      onSuccess: () => queryClient.invalidateQueries(["cards"]),
+    }
+  );
+
+  const trailingActions = (id: string) => (
+    <TrailingActions>
+      <SwipeAction
+        onClick={() => {
+          deleteList(id);
+        }}
+      >
+        <div className="bg-ux-error flex justify-center content-center text-text-white place-items-center rounded-r-2xl">
+          <div className="flex justify-between fixed w-28 ml-7 mr-7 content-center">
+            <p className="bg-white text-links pt-[0.2rem]">Delete</p>
+            <span className="h-6 w-6">
+              <Trash className="stroke-text-white" />
+            </span>
+          </div>
+        </div>
+      </SwipeAction>
+    </TrailingActions>
+  ); //end of delete ist>
+
+  //2. <Change name
 
   //create a binding to store the Name provided by event, obChange. Will be updated as typing. ApiChangeListName will be triggered when clicking enter
   const [inputName, setInputName] = useState("");
 
-  async function ApiChangeListName(current_id: string) {
-    try {
-      const response = await axios.patch(
-        "http://localhost:3000/api/updateListName",
-        { id: current_id, newName: inputName } as UpdateListNameRequestData,
-        { headers: { "Content-Type": "application/json" } }
-      );
-    } catch (err) {
-      console.log(err);
+  //binding, to determine behavior of Card (Title turns into Inputfield, when changingName is set to true. changingName will be set to true on CLick.
+  const [changingName, setChangingName] = useState(false);
+
+  function ApiChangeListName(list_id: string) {
+    return fetch("http:localhost:3000/api/updateListName", {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        id: list_id,
+        newName: inputName,
+      }),
+    }).then((response) => {
+      console.log("updated list with id " + list_id + response);
+      return response.json();
+    });
+  }
+
+  const { mutate: updateListName } = useMutation(
+    (list_id: string) => ApiChangeListName(list_id),
+    {
+      onSuccess: () => queryClient.invalidateQueries(["cards"]), //how can I invalidate a single card?
     }
+  );
+
+  //end of change Name>
+
+  //< Pin list
+  const leadingActions = (list_id: string, pinned: boolean) => (
+    <LeadingActions>
+      <SwipeAction
+        onClick={() => {
+        }}
+      >
+        <div className="bg-primary-default-Solid flex justify-center content-center text-text-white place-items-center rounded-l-2xl">
+          <div className="flex justify-between fixed w-26 ml-7 mr-7 content-center">
+            <span className="h-6 w-6 m-0">
+              <Bookmark />
+            </span>
+            <p className="bg-white text-links pt-[0.2rem]">
+              {clsx(pinned && "unpin", !pinned && "pin")}
+            </p>
+          </div>
+        </div>
+      </SwipeAction>
+    </LeadingActions>
+  );
+  //> End of pin list
+
+  function ApiPnList(list_id: string) {
+    return fetch(`http://localhost:3000/api/deleteList?id=${list_id}`, {
+      method: "PATCH",
+    }).then((response) => {
+      return response.json();
+    });
+  }
+
+  const { mutate: pinList } = useMutation(
+    (list_id: string) => ApiPnList(list_id),
+    {
+      onSuccess: () => queryClient.invalidateQueries(["cards"]),
+    }
+  );
+
+  function ApiUnpinList(list_id: string) {
+    return fetch(`http://localhost:3000/api/deleteList?id=${list_id}`, {
+      method: "PATCH",
+    }).then((response) => {
+      return response.json();
+    });
+  }
+
+  const { mutate: unpinList } = useMutation(
+    (list_id: string) => ApiUnpinList(list_id),
+    {
+      onSuccess: () => queryClient.invalidateQueries(["cards"])
+    }
+  )
+
+  function PinOrUnpin(list_id: string, pinned: boolean) {
+    pinned && pinList(list_id);
+    !pinned && unpinList(list_id);
   }
 
   return (
@@ -89,7 +188,7 @@ export function SingleCard({ data, createNewCard }: CardProps) {
           "border border-secondary-transparent flex flex-col gap-[10px] w-full h-44 p-5 justify-between bg-secondary-transparent",
           createNewCard
             ? "text-primary-transparent"
-            : "text-primary-default-Solid "
+            : "text-primary-default-Solid"
         )}
       >
         <div className="flex flex-col gap-3 rounded-[14px]">
@@ -131,73 +230,3 @@ export function SingleCard({ data, createNewCard }: CardProps) {
     </SwipeableListItem>
   );
 }
-
-function ApiDeleteList(current_id: string) {
-  // A simple DELETE request with fetch
-  fetch(`http://localhost:3000/api/deleteList?id=${current_id}`, {
-    method: "DELETE",
-  }).then((response) => {
-    console.log(response);
-  });
-}
-
-function ApiPnList(current_id: string) {
-  fetch(`http://localhost:3000/api/deleteList?id=${current_id}`, {
-    method: "PATCH",
-  }).then((response) => {
-    console.log(response);
-  });
-}
-
-function ApiUnpinList(current_id: string) {
-  fetch(`http://localhost:3000/api/deleteList?id=${current_id}`, {
-    method: "PATCH",
-  }).then((response) => {
-    console.log(response);
-  });
-}
-
-function PinOrUnpin(id: string, pinned: boolean){
-  (pinned&&ApiPnList(id));
-  (!pinned&&console.log("HUHUHUHUHUH"))
-}
-
-export const leadingActions = (id: string, pinned: boolean) => (
-  <LeadingActions>
-    <SwipeAction
-      onClick={() => {
-        PinOrUnpin(id, pinned);
-        console.log("after click: pinned: " + pinned)
-      }}
-    >
-      <div className="bg-primary-default-Solid flex justify-center content-center text-text-white place-items-center rounded-l-2xl">
-        <div className="flex justify-between fixed w-26 ml-7 mr-7 content-center">
-          <span className="h-6 w-6 m-0">
-            <Bookmark />
-          </span>
-          <p className="bg-white text-links pt-[0.2rem]">{clsx((pinned&&"unpin"),(!pinned&&"pin"))}</p>
-        </div>
-      </div>
-    </SwipeAction>
-  </LeadingActions>
-);
-
-export const trailingActions = (id: string) => (
-  <TrailingActions>
-    <SwipeAction
-      onClick={() => {
-        console.log("CLICKED");
-        ApiDeleteList(id);
-      }}
-    >
-      <div className="bg-ux-error flex justify-center content-center text-text-white place-items-center rounded-r-2xl">
-        <div className="flex justify-between fixed w-28 ml-7 mr-7 content-center">
-          <p className="bg-white text-links pt-[0.2rem]">Delete</p>
-          <span className="h-6 w-6">
-            <Trash className="stroke-text-white" />
-          </span>
-        </div>
-      </div>
-    </SwipeAction>
-  </TrailingActions>
-);
--- src/components/CardsWrapper.tsx
+++ src/components/CardsWrapper.tsx
@@ -1,40 +1,38 @@
-import React, { useEffect, useState } from "react";
+//import functions that return mutation
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useRouter } from "next/router";
 import { SingleCard, UserList, UserLists } from "./Card";
 
-export const returnNodes = (data: UserLists) => {
-  return (data.map((element: UserList) => {
-    return (
-      <SingleCard data={element} key={element.id} createNewCard={false} />
-    )}))
-};
-
-type Props = {
-  id: string
+//send request to the backend
+function askApiForCards(user_id: string) {
+  return axios.get(`/api/getCards?id=${user_id}`).then((res) => {
+    return res.data;
+  });
 }
 
-export const CardsWrapper = ({id}: Props) => {
-
-  ///API-Request:
-  const [data, setData] = useState(null)
-  const [isLoading, setLoading] = useState(false)
-
-  useEffect(() => {
-    setLoading(true)
-    fetch(`/api/getCards?id=${id}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data)
-        setLoading(false)
-      })
-  }, [])
-
-  if (isLoading) return <p>Loading...</p>
-  if (!data) return <p>Press plus button to create Your first list!</p>
-
-  return (
-    <div className="flex flex-col gap-3 pt-[160px]">
-      {returnNodes(data)}
-    </div>
-  );
+type Props = {
+  user_id: string;
 };
 
+//map over data passed as argument, return a card (a Node) for each
+export const returnNodes = (data: UserLists) => {
+  return data.map((element: UserList) => {
+    return <SingleCard data={element} key={element.id} new_card={false} />;
+  });
+};
+
+//render a component with all Nodes included. Based on id provided to You as prop, passed forward to getCards
+export default function CardsWrapper({ user_id }: Props) {
+
+    const { data, isLoading } = useQuery<Array<UserList>>(["cards"], () => askApiForCards(user_id), {
+      enabled: Boolean(user_id),
+    });
+
+  const router = useRouter();
+  return (
+    <div className="flex flex-col gap-3 pt-[160px]">
+      {data ? returnNodes(data) : "isLoading"}
+    </div>
+  );
+}
