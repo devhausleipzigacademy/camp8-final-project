0a06afe231225a89473a9576b2c5b817ae0259ba
ArthiRagothaman <arthi@example.com>
2023-03-23 14:42:26 +0100

    app page issues resolved

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,5 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-<<<<<<< HEAD
-import { SessionProvider } from "next-auth/react";
-export default function App({
-  Component,
-  pageProps: { getSession, ...pageProps },
-}: AppProps) {
-  return (
-    <SessionProvider session={getSession}>
-      <Component {...pageProps} />
-    </SessionProvider>
-=======
 import { Inter, Staatliches } from "@next/font/google";
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -18,6 +7,5 @@ export default function App({ Component, pageProps }: AppProps) {
     <div className="bg-grad-frame h-screen w-auto">
       <Component {...pageProps} />
     </div>
->>>>>>> main
   );
 }
