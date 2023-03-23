b54c16bc9ad130ad983d76e1ab81d283b393ed49
peeler <t.delourme11@gmail.com>
2023-03-23 16:51:24 +0100

    waiting for next auth to be merge to push this issue

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,8 @@
+import { LargeButton } from "@/components/LargeButton";
+import Image from "next/image";
 import Head from "next/head";
+import Link from "next/link";
+import { getSession } from "next-auth/client";
 
 export default function Home() {
   return (
@@ -31,4 +35,21 @@ export default function Home() {
       </div>
     </>
   );
+}
+
+export async function getServerSideProps(context: any) {
+  const session = await getSession(context);
+
+  if (session) {
+    return {
+      redirect: {
+        destination: "/home",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
 }
