91fb36e2b458ec75e970650d9d2e0b668f8bebaf
n1lsw <n1ls@pop-os.localdomain>
2023-03-24 16:45:44 +0100

    Clicking on an item adds it to the database. Alert function, however, do not work. Also, hovering over an item does not change the background color

--- src/components/NewItemInput.tsx
+++ src/components/NewItemInput.tsx
@@ -2,6 +2,7 @@ import { useState, Fragment, ChangeEvent } from "react";
 import { Combobox } from "@headlessui/react";
 import { SmallButton } from "./SmallButton";
 import axios from "axios";
+import ListItem from "./ListItem";
 
 type Items = Array<Item>;
 type Item = {
@@ -21,13 +22,57 @@ export function NewItemInput(): JSX.Element {
       });
   };
 
+  const onSelect = async (value: string) => {
+    try {
+      // Send a POST request to your backend server to add the selected item to the database
+      const response = await axios.post("http://localhost:3000/api/addItem", {
+        query: value,
+        inputList: "6ce77b98-453c-40bb-b090-e41b92ed18b5",
+      });
+
+      // If the request was successful, show a success message
+      if (response.status === 201) {
+        alert(`Successfully added ${value} to the database!`);
+      }
+    } catch (error) {
+      // If there was an error, show an error message
+      alert(`Failed to add ${value} to the database: ${error.message}`);
+    }
+  };
+
   return (
     <Combobox>
       <div className=" relative mt-1">
         <Combobox.Options className=" ui-absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base ">
           {list.map((listItem) => (
-            <Combobox.Option key={listItem} value={listItem} className="">
-              {listItem}
+            <Combobox.Option
+              key={listItem}
+              value={listItem}
+              onClick={() => onSelect(listItem)}
+              className={({ active }) =>
+                `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                  active ? "" : " "
+                }`
+              }
+            >
+              {({ selected, active }) => (
+                <>
+                  <span
+                    className={`block truncate ${
+                      selected ? "font-medium" : "font-normal"
+                    }`}
+                  >
+                    {listItem}
+                  </span>
+                  {selected ? (
+                    <span
+                      className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                        active ? "text-black" : "bg-ux-success"
+                      }`}
+                    ></span>
+                  ) : null}
+                </>
+              )}
             </Combobox.Option>
           ))}
         </Combobox.Options>
