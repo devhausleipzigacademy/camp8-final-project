24b83bffba0e6604120e81fe2582b183f3f469ee
Amyria <54778894+amyria3@users.noreply.github.com>
2023-03-25 13:01:59 +0100

    Add QueryClient
    to use it add
    ```const queryClient = useQueryClient();``` inside of Your component, then code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,11 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Inter, Staatliches } from "@next/font/google";
 import { SessionProvider } from "next-auth/react";
+import {
+	QueryClient,
+	QueryClientProvider,
+} from '@tanstack/react-query'
+const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
