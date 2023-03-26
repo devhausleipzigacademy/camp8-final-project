51042c083e9fbc7c0cbed98baf48fe8fe1569faf
amyria3 <54778894+amyria3@users.noreply.github.com>
2023-03-26 22:12:02 +0200

    works, sorts <3

--- src/components/CardsWrapper.tsx
+++ src/components/CardsWrapper.tsx
@@ -5,29 +5,49 @@ import { useRouter } from "next/router";
 import { SingleCard, UserList, UserLists } from "./SingleCard";
 
 //TO GET ALL THE LISTS
-////send request to the backend
+////Send request to the backend
 function askApiForCards(user_id: string) {
   return axios.get(`/api/getCards?id=${user_id}`).then((res) => {
     return res.data;
   });
 }
 
-////TO CREATE ALL THE CARDS
-//map over data passed as argument, return a card (a Node) for each
-const returnNodes = (allCardsData: UserLists, newCardId: string) => {
-    if (allCardsData){
-    return allCardsData.map((element: UserList) => {
-    return <SingleCard cardData={element} key={element.id} newCardId={newCardId} />;
-  });} else {return <>isLoading</>}
+////Function to sort by (favorite === true) or (favorite === false)
+function yesOrNo(dataArray: UserLists, yesOrNo: boolean, pushTo: UserLists) {
+  dataArray.map((element: UserList) => {
+    if (element.favorite === yesOrNo) {
+      pushTo.push(element);
+    }
+  });
+}
+
+////Take all Data, call sorting algorithm on it, push to a new array, take each element of the array and return a Node
+const returnOrderedNodes = (allCardsData: UserLists, newCardId: string) => {
+  const allCardsSorted: UserLists = [] as UserLists;
+  yesOrNo(allCardsData, false, allCardsSorted);
+  yesOrNo(allCardsData, true, allCardsSorted);
+  if (allCardsData) {
+    return allCardsSorted.map((element: UserList) => {
+      return (
+        <SingleCard cardData={element} key={element.id} newCardId={newCardId} />
+      );
+    });
+  } else {
+    return <>isLoading</>;
+  }
 };
 
-//render a component with all Nodes included. Based on id provided to You as prop, passed forward to getCards
+////Based on a user_id provided to You as prop while rendering
+////Component must first ask Api for all the Data, then pass them to 'returnOrderedNodes()'
+///Api Call is wrapped into a mutation Hook, so we are going to call this mutation Hook instead of the Api itself
+////Render a component with all Nodes included.
 type Props = {
   user_id: string;
   newCardId: string;
 };
 
 export default function CardsWrapper({ user_id, newCardId }: Props) {
+
   const { data: allCards } = useQuery<UserLists>(
     ["cards"],
     () => askApiForCards(user_id),
@@ -38,11 +58,12 @@ export default function CardsWrapper({ user_id, newCardId }: Props) {
 
   const router = useRouter();
 
-  if (!allCards){return(<div>"Waiting for data"</div>)}
-  else {
+  if (!allCards) {
+    return <div>"Waiting for data"</div>;
+  } else {
     return (
-      <div className="flex flex-col gap-3 pt-[160px]">
-        {returnNodes(allCards, newCardId)}
+      <div className="flex flex-col-reverse gap-3 pt-[160px]">
+        {returnOrderedNodes(allCards, newCardId)}
       </div>
     );
   }
