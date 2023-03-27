cec778dcaff0404fbff9510ded8cec6702a530f8
iameru <post@i3o.eu>
2023-03-27 16:41:11 +0200

    i am an idiot

--- src/components/ToggleDarkmode.tsx
+++ src/components/ToggleDarkmode.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Switch } from "@headlessui/react";
+import { useStyleStore } from "@/pages/stores/styleStore";
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
@@ -9,57 +9,48 @@ function classNames(...classes: string[]) {
 type ToggleView = {};
 
 export function ToggleDarkmode(props: ToggleView) {
-  const [theme, setTheme] = useState(false);
+  const { darkMode, toggleDarkMode } = useStyleStore();
+  const [loading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const htmlElement = document.documentElement;
-    if (theme) {
-      htmlElement.classList.add("dark");
-    } else {
-      htmlElement.classList.remove("dark");
-    }
-  }, [theme]);
+    setIsLoading(false);
+  }, [darkMode]);
 
+  if (loading) return null;
   return (
     <div className="py-2 ">
-      <Switch.Group>
-        <Switch
-          checked={theme}
-          onChange={setTheme}
-          className={`w-full relative${
-            theme
-              ? " text-primary-default-Solid bg-secondary-default border-secondary-default border-2 border-spacing-2 "
-              : " text-primary-default-Solid bg-secondary-default border-secondary-default border-2 border-spacing-2 "
-          }
-          relative inline-flex h-full w-full content-center rounded-lg `}
-        >
-          <span className="sr-only ">Toggle Lightmode/Darkmode</span>
-          <div className="flex justify-around w-full relative text-text-white z-10 px-4">
-            <span
-              className={`${
-                theme ? " text-primary-default-Solid" : "text-text-white"
-              }`}
-            >
-              Lightmode
-            </span>
-
-            <span
-              className={`${
-                !theme ? " text-primary-default-Solid" : "text-text-white"
-              }`}
-            >
-              Darkmode
-            </span>
-          </div>
+      <Switch
+        checked={!darkMode}
+        onChange={toggleDarkMode}
+        className="w-full relative inline-flex h-full content-center rounded-lg text-primary-default-Solid bg-secondary-default border-secondary-default border-2 border-spacing-2 "
+      >
+        <span className="sr-only ">Toggle Lightmode/Darkmode</span>
+        <div className="flex justify-around w-full relative text-text-white z-10 px-4">
           <span
-            aria-hidden="true"
-            className={`absolute ${
-              theme ? "translate-x-full" : "-translate-x-0"
-            }
+            data-headlessui-state="on"
+            className={`${
+              darkMode ? " text-primary-default-Solid" : "text-text-white"
+            }`}
+          >
+            Lightmode
+          </span>
+
+          <span
+            className={`${
+              !darkMode ? " text-primary-default-Solid" : "text-text-white"
+            }`}
+          >
+            Darkmode
+          </span>
+        </div>
+        <span
+          aria-hidden="true"
+          className={`absolute ${
+            darkMode ? "translate-x-full" : "-translate-x-0"
+          }
             pointer-events-none text-text-white inline-block h-full w-[50%] py-2 transform rounded-md bg-primary-default-Solid transition duration-300 ease-in-out `}
-          ></span>
-        </Switch>
-      </Switch.Group>
+        ></span>
+      </Switch>
     </div>
   );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,29 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Inter, Staatliches } from "@next/font/google";
 import { SessionProvider } from "next-auth/react";
+import {useStyleStore} from "./stores/styleStore";
+import clsx from "clsx";
+import {useEffect} from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  const {darkMode} = useStyleStore()
+
+  useEffect(() => {
+  
+    const htmlElement = document.getElementsByTagName("html")[0];
+    if (darkMode) {
+      htmlElement.classList.add("dark");
+    } else {
+      htmlElement.classList.remove("dark");
+    }
+  }, [darkMode])
+
+
   return (
-    <div className="bg-grad-frame h-screen w-auto">
+    <div className={clsx("bg-grad-frame h-screen w-auto",
+      darkMode && "dark"
+    )}>
       <SessionProvider>
         <Component {...pageProps} />
       </SessionProvider>
