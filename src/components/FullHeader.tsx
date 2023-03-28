ab6b9e010d7baa8b94410dabf3c5e00500f9720b
Marhamat <M.berhan@gmx.de>
2023-03-28 13:31:04 +0200

    added

--- src/components/FullHeader.tsx
+++ src/components/FullHeader.tsx
@@ -3,18 +3,18 @@ import { IconButton } from "./IconButton";
 import { HeaderTitle } from "./HeaderTitle";
 
 type FullHeader = {
-  classes: string;
-  name: string;
+	classes: string;
+	name: string;
 };
 
 export function FullHeader(props: FullHeader) {
-  return (
-    <div className="flex items-center gap-24">
-      <HeaderTitle
-        classes="text-title text-primary-default-Solid "
-        name={""}
-      ></HeaderTitle>
-      <IconButton classes="w-12 h-12"></IconButton>
-    </div>
-  );
+	return (
+		<div className="flex items-center gap-24">
+			<HeaderTitle
+				classes="text-title text-4xl text-primary-default-Solid "
+				name={""}
+			></HeaderTitle>
+			<IconButton classes="w-12 h-12"></IconButton>
+		</div>
+	);
 }
--- src/components/HeaderTitle.tsx
+++ src/components/HeaderTitle.tsx
@@ -2,16 +2,19 @@ import clsx from "clsx";
 import React from "react";
 
 type HeaderTitle = {
-  classes: string;
-  name: string;
+	classes: string;
+	name: string;
 };
 
 export function HeaderTitle(props: HeaderTitle) {
-  return (
-    <h1
-      className={clsx("text-title text-primary-default-Solid", props.classes)}
-    >
-      Hey, beautiful {props.name}!
-    </h1>
-  );
+	return (
+		<h1
+			className={clsx(
+				"text-title text-primary-default-Solid",
+				props.classes
+			)}
+		>
+			Hey, beautiful {props.name}!
+		</h1>
+	);
 }
