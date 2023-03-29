0431109fcd261e3874803247c1b085a70079ff2c
amyria3 <54778894+amyria3@users.noreply.github.com>
2023-03-29 17:51:34 +0200

    create-new-card works, button has the right position

--- src/pages/home/apiCalls.tsx
+++ src/pages/home/apiCalls.tsx
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+  //create a binding to store the Name provided by event, obChange. Will be updated as typing. ApiChangeListName will be triggered when clicking enter
+export const newCardId = (user_id: string) => {
+    return axios.post(`http://localhost:3000/api/createList?id=${user_id}`);
+  };
+
 export function askApiForCards(user_id: string) {
   return axios.get(`/api/getCards?id=${user_id}`).then((res) => res.data);
 }
@@ -23,7 +28,7 @@ export function apiChangeListName(list_id: string, updatedName: string) {
   });
 }
 
-export function apiPinList(list_id: string) {  
+export function apiPinList(list_id: string) {
   return fetch(`http://localhost:3000/api/pinList?id=${list_id}`, {
     method: "PATCH"
   });
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -7,19 +7,15 @@ import Head from "next/head";
 import { PlusButton } from "@/components/PlusButton";
 import { SearchBar } from "@/components/SearchBar";
 import { FullHeader } from "@/components/FullHeader";
+import { newCardId } from "./apiCalls";
 
 export default function Home(id_user: string) {
   //link to queryClient in app.tsx
   const queryClient = useQueryClient();
 
-  //create a binding to store the Name provided by event, obChange. Will be updated as typing. ApiChangeListName will be triggered when clicking enter
-  const newCardId = (id_user: string) => {
-    return axios.post(`http://localhost:3000/api/createList?id=${id_user}`);
-  };
-
   //updates data linked to render all cards automatically as newCardId is called
   const { mutate: createNewCard } = useMutation({
-    mutationFn: (list_id: string) => newCardId(list_id),
+    mutationFn: (id_user: string) => newCardId(id_user),
     onSuccess: () => queryClient.invalidateQueries(["cards"]),
   });
 
@@ -29,7 +25,7 @@ export default function Home(id_user: string) {
         <FullHeader classes={""} name={""}></FullHeader>
       </div>
       <div className="row-span-1 h-full overflow-y-scroll">
-        <CardsWrapper user_id="43b20ffc-ceea-43d5-b08c-9a1a6e4a1f98" />
+        <CardsWrapper user_id={id_user} />
       </div>
       <PlusButton
     onClick={() => {
