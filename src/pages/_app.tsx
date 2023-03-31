9b90756007d0334392c5ae78af636e1ca58da794
NoHara42 <mailned42@gmail.com>
2023-03-31 14:06:39 +0200

    themeprovider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -29,7 +29,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <ThemeProvider>
+      <ThemeProvider attribute="class">
         <QueryClientProvider client={queryClient}>
           <SessionProvider>
             <Component {...pageProps} />
